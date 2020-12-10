8987ab95a9da747eae877a28227004b152bf41f7
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2020-12-10 12:57:08 -0600

    update all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import notes from "../notes";
 
 function App(){
     return (
         <div>
             <Header/>
-            <Note/>
+            {notes.map(note =>(
+                <Note
+                    key = {note.id}
+                    title = {note.title}
+                    content = {note.content}
+                />
+            ))}
             <Footer/>
         </div>
+        
     )
 }
 
